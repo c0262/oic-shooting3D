fbeead5d4743f944d1c43ba32f6b1e9a5abd5627
c0262 <c0262@oic.jp>
2022-05-27 10:40:28 +0900

    9-4まで

--- Project/Stage.h
+++ Project/Stage.h
@@ -1,20 +1,29 @@
 #pragma once
 
 #include "GameDefine.h"
+#include "Enemy.h"
 
 // スクロール速度
 #define		SCROLL_SPEED	(1.0f)
+typedef struct tag_ENEMYSTRAT {
+	int						Count;
+	float*					PosX;
+	float*					Scroll;
+}ENEMYSTRAT;
 
 class CStage{
 private:
 	CSprite3D			m_BG;
 	float				m_Scroll;
+	ENEMYSTRAT*			m_pEnemyStart;
+	CMeshContainer		m_EnemyMesh;
+	int					m_EnemyNo;
 public:
 	CStage();
 	~CStage();
 	bool Load();
-	void Initialize();
-	void Update();
+	void Initialize(ENEMYSTRAT* pSt);
+	void Update(CEnemy* ene,int ecnt);
 	void Render();
 	void RenderDebugText();
 	void Release();
